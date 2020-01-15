70b7960963902d10aefee1156bd890553b9f8372
Essential08 <48437597+Essential08@users.noreply.github.com>
2020-01-15 17:24:27 +0100

    Contentpagina Standaard

---
+++ Keukenhof/Keukenhof/Controllers/ContentPaginaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Keukenhof.Controllers
+{
+    public class ContentPaginaController : Controller
+    {
+        // GET: ContentPagina
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
