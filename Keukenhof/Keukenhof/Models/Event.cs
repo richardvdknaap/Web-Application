c49f37988e6b631236ff81cccd0e49b99ef275f5
MineWhiskyCraft <48437330+MineWhiskyCraft@users.noreply.github.com>
2020-01-17 14:34:31 +0100

    DB werkt niet

---
+++ Keukenhof/Keukenhof/Models/Event.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Keukenhof.Models
+{
+    public class Event
+    {
+        public int EventId { get; set; }
+        public string Naam  { get; set; }
+        public string Omschrijving { get; set; }
+        public string Datum { get; set; }
+        public string FotoURL { get; set; }
+    }
+}
