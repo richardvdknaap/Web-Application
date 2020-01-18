e95980dc8cd785e5ad2df327e1e4a7771f3c5f5e
Richard <richard.vd.knaap@outlook.com>
2020-01-18 22:57:51 +0100

    Making context and an event class to test databases

---
+++ Keukenhof_2.0/Keukenhof_2.0/Models/Event.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keukenhof_2._0.Models
+{
+    public class Event
+    {
+        int id { get; set; }
+        string naam { get; set; }
+    }
+}
---
+++ Keukenhof_2.0/Keukenhof_2.0/Models/KeukenhofContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Keukenhof_2._0.Models
+{
+    public class KeukenhofContext : DbContext
+    {
+        public KeukenhofContext(DbContextOptions<KeukenhofContext> options)
+         : base(options)
+        { }
+        public DbSet<Event> Events { get; set; }
+    }
+}
