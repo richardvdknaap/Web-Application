fbde1762151478773e460074cfe90151fc110ef0
18050794 <18050794@student.hhs.nl>
2020-01-20 14:48:53 +0100

    Add first test

--- Keukenhof_2.0/WebXUnitTest/UnitTest1.cs
+++ Keukenhof_2.0/WebXUnitTest/UnitTest1.cs
@@ -1,3 +1,5 @@
+using Keukenhof_2._0.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using Xunit;
 
@@ -5,10 +7,20 @@ namespace WebXUnitTest
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        private HomeController _homeController;
+        
+        public UnitTest1()
         {
+            _homeController = new HomeController();
+        }
 
+        [Fact]
+        public void IndexActionReturnsIndexView()
+        {
+            var result = _homeController.Index() as ViewResult;
+            Assert.Equal("Index", result.ViewName);
+            Console.WriteLine(result);
+            Console.ReadKey(true);
         }
     }
 }
