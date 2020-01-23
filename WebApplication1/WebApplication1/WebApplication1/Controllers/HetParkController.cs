8f5bc18f910894bdd87c6083ff681af35f7c5a09
Sheriyar1 <54804024+Sheriyar1@users.noreply.github.com>
2020-01-23 10:43:21 +0100

    Nieuwe bestand lan

---
+++ WebApplication1/WebApplication1/WebApplication1/Controllers/HetParkController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keukenhof.Controllers
+{
+    public class HetParkController : Controller
+    {
+        // GET: HetPark
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/WebApplication1/Controllers/InfoController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keukenhof.Controllers
+{
+    public class InfoController : Controller
+    {
+        // GET: Info
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
