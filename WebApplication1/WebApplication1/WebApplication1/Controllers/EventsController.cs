48e0d051241d56f2336cb9ae12be33cbd633e785
Sheriyar1 <54804024+Sheriyar1@users.noreply.github.com>
2020-01-23 16:33:07 +0100

    Namen

--- WebApplication1/WebApplication1/WebApplication1/Controllers/EventsController.cs
+++ WebApplication1/WebApplication1/WebApplication1/Controllers/EventsController.cs
@@ -4,9 +4,10 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Keukenhof.Models;
+using WebApplication1.Models;
 
-namespace Keukenhof.Controllers
+
+namespace WebApplication1.Controllers
 {
     public class EventsController : Controller
     {
--- WebApplication1/WebApplication1/WebApplication1/Controllers/PanelController.cs
+++ WebApplication1/WebApplication1/WebApplication1/Controllers/PanelController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
 
 namespace WebApplication1.Controllers
 {
